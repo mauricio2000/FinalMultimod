1f534772db80763dfba4a90b12c3d976576dae18
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-11-07 16:45:08 -0500

    Create luces.py

---
+++ luces.py
@@ -0,0 +1,68 @@
+import paho.mqtt.client as paho
+import time
+import streamlit as st
+import json
+values = 0.0
+act1="OFF"
+
+def on_publish(client,userdata,result):             #create function for callback
+    print("el dato ha sido publicado \n")
+    pass
+
+def on_message(client, userdata, message):
+    global message_received
+    time.sleep(2)
+    message_received=str(message.payload.decode("utf-8"))
+    st.write(message_received)
+
+        
+
+
+broker="157.230.214.127"
+port=1883
+client1= paho.Client("ESTE_ES_MAURI2")
+client1.on_message = on_message
+
+
+
+st.title("MQTT Control")
+
+if st.button('Encender luces'):
+    act1="Encendido"
+    client1= paho.Client("ESTE_ES_MAURI2")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act1":act1})
+    ret= client1.publish("mauri1", message)
+ 
+    #client1.subscribe("Sensores")
+    
+    
+else:
+    st.write('')
+
+if st.button('Apagar Luces'):
+    act1="Apagado"
+    client1= paho.Client("ESTE_ES_MAURI2")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)  
+    message =json.dumps({"Act1":act1})
+    ret= client1.publish("mauri1", message)
+  
+    
+else:
+    st.write('')
+
+values = st.slider('Selecciona el rango de valores',0.0, 100.0)
+st.write('Values:', values)
+
+if st.button('Enviar valor analógico'):
+    client1= paho.Client("ESTE_ES_MAURI2")                           
+    client1.on_publish = on_publish                          
+    client1.connect(broker,port)   
+    message =json.dumps({"Analog": float(values)})
+    ret= client1.publish("mauri2", message)
+    
+ 
+else:
+    st.write('')
