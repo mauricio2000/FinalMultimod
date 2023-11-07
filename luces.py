d62670543b6f229eba8c821b84883a859471dbcf
Mauricio <41337819+mauricio2000@users.noreply.github.com>
2023-11-07 17:24:06 -0500

    Update luces.py

--- luces.py
+++ luces.py
@@ -2,6 +2,10 @@ import paho.mqtt.client as paho
 import time
 import streamlit as st
 import json
+import pytesseract
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
 values = 0.0
 act1="OFF"
 act2="Cerrao"
